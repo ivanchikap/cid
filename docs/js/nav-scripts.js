891ab43f50b39f95c0069d8b1c905fc18fed50b2
ivanchikap <anton.ivanchik@gmail.com>
2020-05-07 22:06:04 +0300

    07052020

---
+++ docs/js/nav-scripts.js
@@ -0,0 +1,16 @@
+(function () {
+    let nav = $('.nav');
+
+    // .nav__btn - кнопка меню
+    $('.nav__btn').click(function () {
+        let curHeight = nav.height();
+        let autoHeight = nav.css('height', 'auto').height();
+        if (nav.hasClass('nav--active')) {
+            nav.removeClass('nav--active');
+            nav.height(curHeight).animate({height: 0}, 0)
+        } else {
+            nav.addClass('nav--active');
+            nav.height(curHeight).animate({height: autoHeight}, 0)
+        }
+    })
+})();
